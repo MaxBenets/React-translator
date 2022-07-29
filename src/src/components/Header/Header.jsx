b9c7b24fc0d7597c472175950375e80894507702
Maksym <98233906+MaxBenets@users.noreply.github.com>
2022-07-30 00:28:10 +0300

    Delete src directory

--- src/src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./Header.module.css"
-
-export const Header = () => {
-    return(
-        <header className={s.header}>
-            <h1 className={s.h1}>React Translate</h1>
-        </header>
-    )
-}
